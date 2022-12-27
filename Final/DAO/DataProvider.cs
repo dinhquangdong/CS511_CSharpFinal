52b774bb3c116364268d87453426c6bd82ea4d55
vanchinh12042002 <truongchinh708@gmail.com>
2022-12-27 10:04:18 +0700

    Them DataProvider link hd: https://howkteam.vn/course/lap-trinh-phan-mem-quan-ly-quan-cafe-voi-c-winform/tao-lop-dataprovider-cho-phan-mem-quan-ly-quan-cafe-voi-c-winform-1239

---
+++ Final/DAO/DataProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.DAO
+{
+    public class DataProvider
+    {
+        private string connectionSTR = "Data Source=.\\sqlexpress;Initial Catalog=TimTruongDaiHoc;Integrated Security=True";
+        public DataTable ExcuteQuery(string query)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dt);
+                connection.Close();
+            }
+            return dt;
+        }
+
+        public int ExcuteNonQuery(string query)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Close();
+                data = command.ExecuteNonQuery();
+            }
+            return data;
+        }
+
+        public object ExcuteScalar(string query)
+        {
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Close();
+                data = command.ExecuteScalar();
+            }
+            return data;
+        }
+
+    }
+}
--- Final/MainScreen.Designer.cs
+++ Final/MainScreen.Designer.cs
@@ -31,11 +31,11 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -70,12 +70,22 @@
             this.panel2.Size = new System.Drawing.Size(308, 711);
             this.panel2.TabIndex = 1;
             // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(0, 451);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(308, 68);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Tìm kiếm theo điểm";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(0, 385);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(302, 68);
+            this.button4.Size = new System.Drawing.Size(308, 68);
             this.button4.TabIndex = 3;
             this.button4.Text = "Tìm kiếm theo Tổ hợp môn";
             this.button4.UseVisualStyleBackColor = true;
@@ -85,7 +95,7 @@
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(0, 321);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(302, 68);
+            this.button3.Size = new System.Drawing.Size(308, 68);
             this.button3.TabIndex = 2;
             this.button3.Text = "Tìm kiếm theo nghành";
             this.button3.UseVisualStyleBackColor = true;
@@ -95,7 +105,7 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(0, 643);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(302, 68);
+            this.button2.Size = new System.Drawing.Size(308, 68);
             this.button2.TabIndex = 1;
             this.button2.Text = "Thông tin bản thân";
             this.button2.UseVisualStyleBackColor = true;
@@ -103,22 +113,12 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(0, 258);
+            this.button1.Location = new System.Drawing.Point(0, 85);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(302, 68);
+            this.button1.Size = new System.Drawing.Size(308, 68);
             this.button1.TabIndex = 0;
             this.button1.Text = "Tất cả các trường";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(0, 451);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(302, 68);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Tìm kiếm theo điểm";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // MainScreen
             // 
