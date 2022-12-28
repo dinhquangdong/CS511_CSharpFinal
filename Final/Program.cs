c7ac92438f827bda392acd4a0772980d453cbf15
Dong Quang <quangdong02112002@outlook.com>
2022-12-28 08:12:19 +0700

    oke 11

--- Final/Program.cs
+++ Final/Program.cs
@@ -17,7 +17,7 @@ namespace Final
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainScreen());
+            Application.Run(new InsertData());
         }
     }
 }
