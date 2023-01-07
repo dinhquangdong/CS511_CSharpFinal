ef7b80991a2042c24ac616531ea037d148ca2953
Dong Quang <quangdong02112002@outlook.com>
2023-01-07 18:13:24 +0700

    oke 1

--- Final/UserControlNganh.Designer.cs
+++ Final/UserControlNganh.Designer.cs
@@ -37,7 +37,7 @@
             this.labelTenNganh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTenNganh.Location = new System.Drawing.Point(9, 11);
             this.labelTenNganh.Name = "labelTenNganh";
-            this.labelTenNganh.Size = new System.Drawing.Size(195, 104);
+            this.labelTenNganh.Size = new System.Drawing.Size(184, 104);
             this.labelTenNganh.TabIndex = 0;
             this.labelTenNganh.Text = "Khoa học máy tính";
             this.labelTenNganh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -46,9 +46,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Controls.Add(this.labelTenNganh);
             this.Name = "UserControlNganh";
-            this.Size = new System.Drawing.Size(216, 130);
+            this.Size = new System.Drawing.Size(205, 130);
             this.Load += new System.EventHandler(this.UserControlNganh_Load);
             this.ResumeLayout(false);
 
