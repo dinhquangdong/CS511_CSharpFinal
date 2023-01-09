5866002b2ad1b068ac81141bb785b90d939bbee2
vanchinh12042002 <truongchinh708@gmail.com>
2023-01-09 21:58:37 +0700

    r

--- Final/UserControlTruong.Designer.cs
+++ Final/UserControlTruong.Designer.cs
@@ -44,6 +44,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(124, 133);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.UserControlTruong_Click);
             // 
             // labelName
             // 
