42ab0ce9f100b3a5045071f5eb360b668074ef13
Dong Quang <quangdong02112002@outlook.com>
2023-01-09 21:11:03 +0700

    1

---
+++ Final/ChiTietTruong.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Final
+{
+    partial class ChiTietTruong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChiTietTruong));
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelMaTruong = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.labelDiaChi = new System.Windows.Forms.Label();
+            this.labelSoDienThoai = new System.Windows.Forms.Label();
+            this.labelSaoDanhGia = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.BackgroundImage")));
+            this.pictureBoxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(29, 35);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(430, 351);
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.Color.Blue;
+            this.labelName.Location = new System.Drawing.Point(483, 35);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(571, 37);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Trường Đại học Công nghệ thông tin";
+            // 
+            // labelMaTruong
+            // 
+            this.labelMaTruong.AutoSize = true;
+            this.labelMaTruong.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMaTruong.Location = new System.Drawing.Point(32, 410);
+            this.labelMaTruong.Name = "labelMaTruong";
+            this.labelMaTruong.Size = new System.Drawing.Size(209, 31);
+            this.labelMaTruong.TabIndex = 2;
+            this.labelMaTruong.Text = "Mã trường: QSC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Website:";
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel.Location = new System.Drawing.Point(143, 451);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(201, 31);
+            this.linkLabel.TabIndex = 4;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "www.uit.edu.vn";
+            // 
+            // labelDiaChi
+            // 
+            this.labelDiaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDiaChi.Location = new System.Drawing.Point(32, 495);
+            this.labelDiaChi.MaximumSize = new System.Drawing.Size(430, 64);
+            this.labelDiaChi.Name = "labelDiaChi";
+            this.labelDiaChi.Size = new System.Drawing.Size(430, 64);
+            this.labelDiaChi.TabIndex = 5;
+            this.labelDiaChi.Text = "Địa chỉ: Khu phố 6, P.Linh Trung, Tp.Thủ Đức, Tp.Hồ Chí Minh.";
+            // 
+            // labelSoDienThoai
+            // 
+            this.labelSoDienThoai.AutoSize = true;
+            this.labelSoDienThoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSoDienThoai.Location = new System.Drawing.Point(36, 584);
+            this.labelSoDienThoai.Name = "labelSoDienThoai";
+            this.labelSoDienThoai.Size = new System.Drawing.Size(308, 31);
+            this.labelSoDienThoai.TabIndex = 6;
+            this.labelSoDienThoai.Text = "Điện thoại:  0123456789";
+            // 
+            // labelSaoDanhGia
+            // 
+            this.labelSaoDanhGia.AutoSize = true;
+            this.labelSaoDanhGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSaoDanhGia.Location = new System.Drawing.Point(36, 626);
+            this.labelSaoDanhGia.Name = "labelSaoDanhGia";
+            this.labelSaoDanhGia.Size = new System.Drawing.Size(212, 31);
+            this.labelSaoDanhGia.TabIndex = 7;
+            this.labelSaoDanhGia.Text = "Đánh giá: 4.5 / 5";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(490, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(849, 265);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(350, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 58);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(484, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 31);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Danh sách các ngành";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(493, 445);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(845, 246);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // ChiTietTruong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 711);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.labelSaoDanhGia);
+            this.Controls.Add(this.labelSoDienThoai);
+            this.Controls.Add(this.labelDiaChi);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelMaTruong);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Name = "ChiTietTruong";
+            this.Text = "ChiTietTruong";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelMaTruong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.Label labelDiaChi;
+        private System.Windows.Forms.Label labelSoDienThoai;
+        private System.Windows.Forms.Label labelSaoDanhGia;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
